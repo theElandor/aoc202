727c86892cd8fc8061ff789a50c1133de3efc56e
Matteo <matteolugli18@gmail.com>
2024-12-01 10:41:18 +0100

    refactored

--- day1/a.py
+++ day1/a.py
@@ -3,5 +3,4 @@ with open("input.txt") as f:
     items = [[int(y) for y in x.split("   ")] for x in grid]
     l1 = [x for x,y in items]
     l2 = [y for x,y in items]
-    tot = 0
     print(sum([abs(x-y) for x, y in zip(sorted(l1), sorted(l2))]))
