3eeb530057611d2c45f2a442c9f9d9b9b48719d6
Matteo <matteolugli18@gmail.com>
2024-12-10 11:54:55 +0100

    fast day

---
+++ day10/a.py
@@ -0,0 +1,32 @@
+import sys
+directions = [(1,0),(0,1),(0,-1),(-1,0)]
+ends = []
+def ok(grid, i, j):
+	if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+		return False
+	if grid[i][j] == ".":
+		return False
+	return True
+	
+def check(grid, i, j, visited):
+	# dfs, every time we reach 9, if not seen, count 1
+	if grid[i][j] == 9:
+		ends.append((i,j))
+	current = grid[i][j]
+	for di, dj in directions:
+		if ok(grid, i+di, j+dj) and grid[i+di][j+dj] == current+1 and (i+di,j+dj) not in visited:
+			visited.add((i+di, j+dj))
+			check(grid, i+di, j+dj, visited)
+
+				
+filename = sys.argv[1]
+with open(filename) as f:
+	grid = [[int(x) for x in list(line)] for line in f.read().splitlines()]
+	total = 0
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			if grid[i][j] == 0:
+				check(grid, i,j, set())
+				total += len(ends)
+				ends.clear()
+	print(total)
